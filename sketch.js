0fd12dccae31c6deacfae5145a26e5f4b2d4d956
Trojwolf <92826437+Trojwolf@users.noreply.github.com>
2021-11-11 16:24:04 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function preload(){
+  teapot=loadModel("https://raw.githubusercontent.com/processing/p5.js/main/test/manual-test-examples/webgl/assets/teapot-ascii.stl",true);
+}
+  
+function setup() {
+  createCanvas(windowWidth,windowHeight,WEBGL);
+}
+function draw() {
+  background(0);
+  push();
+  scale(3);
+  rotateX(PI/2);
+  rotateZ(frameCount*0.01);
+  noStroke();
+  ambientLight(100);
+  directionalLight(130,200,255,150,20,50)
+  model(teapot);
+  pop();
+  normalMaterial();
+  translate(-width/2,-height/2);
+  ellipse(mouseX,mouseY,100,100);
+}
